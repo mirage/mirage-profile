edd15cdb9d0c2dadf43989460a0cbdad7ab1ecef
Thomas Leonard <talex5@gmail.com>
2014-12-04 15:54:19 +0000

    Avoid O_CLOEXEC to support OCaml 4.00

--- unix/mProf_unix.ml
+++ unix/mProf_unix.ml
@@ -6,7 +6,8 @@ type log_buffer = (char, int8_unsigned_elt, c_layout) Array1.t
 external timestamper : log_buffer -> int -> unit = "stub_mprof_get_monotonic_time"
 
 let mmap_buffer ~size path =
-  let fd = Unix.(openfile path [O_RDWR; O_CREAT; O_TRUNC; O_CLOEXEC] 0o644) in
+  let fd = Unix.(openfile path [O_RDWR; O_CREAT; O_TRUNC] 0o644) in
+  Unix.set_close_on_exec fd;
   Unix.ftruncate fd size;
   let ba = Array1.map_file fd char c_layout true size in
   Unix.close fd;
