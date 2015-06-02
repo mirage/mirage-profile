e72d6c07f5e866c8b9c0a054582ef6dc649bef0f
Thomas Leonard <talex5@gmail.com>
2015-06-02 11:13:46 +0100

    Allow getting and setting the value of a counter

--- lib/counter.ml
+++ lib/counter.ml
@@ -2,9 +2,16 @@
 
 type t = {
   name : string;
+  mutable value : int;
 }
 
-let make ~name = { name }
+let create ?(init=0) ~name () = { name; value = init }
+let make ~name = create ~name ()
 
 let increase m amount =
+  m.value <- m.value + amount;
   Trace.note_increase m.name amount
+
+let value m = m.value
+
+let set_value m v = increase m (v - m.value)
--- lib/counter.mli
+++ lib/counter.mli
@@ -4,7 +4,11 @@
 
 type t
 
+val create : ?init:int -> name:string -> unit -> t
 val make : name:string -> t
+
+val value : t -> int
+val set_value : t -> int -> unit
 
 val increase : t -> int -> unit
 (** Record a change in the value of the metric. The change can be negative. *)
