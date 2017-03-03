e2eb6fd440b0e61e557c65dc531a168f12397d69
Chris Fegan <chrisfegan@nbnco.com.au>
2017-03-04 00:24:23 +1100

    Conditional compilation based using cppo

---
+++ lib/trace.cppo.ml
@@ -0,0 +1,5 @@
+#ifdef USE_TRACING
+#include "trace_enabled.ml"
+#else
+#include "trace_stubs.ml"
+#endif
---
+++ lib/trace.cppo.mli
@@ -0,0 +1,5 @@
+#ifdef USE_TRACING
+#include "trace_enabled.mli"
+#else
+#include "trace_stubs.mli"
+#endif
