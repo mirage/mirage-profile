c1385e312cfc09b120eed51a920b1056a640b32c
Chris Fegan <chrisfegan@nbnco.com.au>
2017-03-04 05:00:15 +1100

    Detecting use of tracing in pkg.ml

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -1,15 +1,21 @@
 #!/usr/bin/env ocaml
 #use "topfind"
 #require "topkg"
+#require "unix"
 open Topkg
 
 let xen = Conf.with_pkg ~default:false "xen"
 
 let () =
-  Pkg.describe "mirage-profile" @@ fun c ->
-  let xen = Conf.value c xen in
+  Pkg.describe "mirage-profile" @@ fun c -> 
+  let use_tracing =
+    match Unix.system("ocamlfind query lwt.tracing > /dev/null 2>&1") with
+    | Unix.WEXITED 0 -> true
+    | Unix.WEXITED _ -> false
+    | _ -> failwith "ocamlfind failed!" in
+  let xen = Conf.value c xen && use_tracing in
   Ok [
     Pkg.mllib ~api:["MProf"] "lib/mProf.mllib" ; 
-    Pkg.mllib "unix/mProf_unix.mllib" ; 
+    Pkg.mllib ~cond:use_tracing "unix/mProf_unix.mllib" ; 
     Pkg.mllib ~cond:xen "xen/mProf_xen.mllib" ; 
   ]
