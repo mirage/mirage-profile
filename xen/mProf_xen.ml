03c9781d1830e88bc525d3ab09e5be179897c65a
Thomas Leonard <talex5@gmail.com>
2019-05-04 12:09:49 +0100

    Update to new grant API
    When mirage-profile was a single opam package we couldn't have
    mirage-profile-xen depend on modules that depended in turn on
    mirage-profile. Now these are separate opam packages we can, so use the
    new APIs directly.

--- xen/mProf_xen.ml
+++ xen/mProf_xen.ml
@@ -5,6 +5,9 @@ type log_buffer = (char, int8_unsigned_elt, c_layout) Array1.t
 
 external get_monotonic_time : unit -> int64 = "caml_get_monotonic_time"
 
+module Export = OS.Xen.Export
+module Xs = OS.Xs
+
 let timestamper buf off =
   EndianBigstring.LittleEndian.set_int64 buf off (get_monotonic_time ())
 
@@ -13,42 +16,20 @@ let make_shared_buffer ~size =
   let n_pages = round_to_page_size size / round_to_page_size 1 in
   get n_pages
 
-type gntref = int
-
-(* ocaml-gnt and mirage-xen both depend on mirage-platform, so pass them in here
- * to avoid a cycle (alternatively, we could move mirage-profile.xen to a separate
- * OPAM package). *)
-module type GNTSHR = sig
-  val get : unit -> gntref Lwt.t
-  val grant_access : domid:int -> writable:bool -> gntref -> Io_page.t -> unit
-end
-
-module type XS = sig
-  type client
-  type handle
-
-  val make : unit -> client Lwt.t
-  val read : handle -> string -> string Lwt.t
-  val write : handle -> string -> string -> unit Lwt.t
-  val immediate : client -> (handle -> 'a Lwt.t) -> 'a Lwt.t
-  val transaction : client -> (handle -> 'a Lwt.t) -> 'a Lwt.t
-  val getdomainpath : handle -> int -> string Lwt.t
-end
-
-let share_with (module Gntshr : GNTSHR) (module Xs : XS) ~domid buffer =
+let share_with ~domid buffer =
   let pages = Io_page.to_pages buffer in
   let open Lwt in
 
   let refs =
     pages |> Lwt_list.map_s (fun page ->
-      Gntshr.get () >>= fun gnt ->
-      Gntshr.grant_access ~domid ~writable:false gnt page;
+      Export.get () >>= fun gnt ->
+      Export.grant_access ~domid ~writable:false gnt page;
       return gnt
     ) in
   refs >>= fun refs ->
 
   let ring_ref = refs
-    |> List.map string_of_int
+    |> List.map OS.Xen.Gntref.to_string
     |> String.concat "," in
 
   Xs.make ()
